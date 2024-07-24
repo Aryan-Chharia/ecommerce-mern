634c972fb85232c6831538dbd8262cb798e841b7
Aryan-Chharia <aryan.chharia@gmail.com>
2024-07-25 01:00:36 +0530

    First Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
